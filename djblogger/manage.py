4556769b077ddf731c8532b6bc9939d8e12e661c
Sagar Raturi <sagarraturi85516@gmail.com>
2026-05-31 04:17:09 +0530

    Add black and flak8

--- djblogger/manage.py
+++ djblogger/manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 from djblogger.settings import base
@@ -9,9 +10,9 @@ def main():
     """Run administrative tasks."""
 
     if base.DEBUG:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblogger.settings.local')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djblogger.settings.local")
     else:
-        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djblogger.settings.production')
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djblogger.settings.production")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -23,5 +24,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
